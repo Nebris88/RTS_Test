c03095a57577eb298d063197e282facb75005cab
Nebris <nebris88@gmail.com>
2022-04-01 01:04:34 -0400

    Test answers

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RTS_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestAboveBelow(new int[] { 1, 5, 2, 1, 10 }, 6);
+            TestAboveBelow(new int[] { }, 0);
+            TestAboveBelow(new int[] { 0, 1, 2, 3, 4, 5, 6 }, 3);
+            TestAboveBelow(new int[] { 10, 9, 8, 7, 6 }, 5);
+            TestAboveBelow(new int[] { 10, 9, 8, 7, 6 }, 8);
+            TestAboveBelow(new int[] { 10, 9, 8, 7, 6 }, 11);
+
+            TestStringRotation("MyString", 1);
+            TestStringRotation("MyString", 2);
+            TestStringRotation("MyString", 5);
+            TestStringRotation("MyString", 10);
+            TestStringRotation("MyString", 0);
+            //TestStringRotation("MyString", -2);
+            //TestStringRotation("", 1);
+        }
+
+        private static void TestAboveBelow(int[] ints, int value)
+        {
+            Console.WriteLine($"Testing Above/Below with values {ConvertIntArray(ints)} and {value}");
+            PrintAboveBelowDictionary(Test.aboveBelow(ints, value));
+        }
+
+        private static void TestStringRotation(string word, int rotation)
+        {
+            Console.WriteLine($"Testing StringRotation with values {word} and {rotation}");
+            Console.WriteLine(Test.stringRotation(word, rotation));
+        }
+
+        //For displaying the int array contents in console.
+        private static string ConvertIntArray(int[] ints)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+            for (int i = 0; i < ints.Length; i++)
+            {
+                sb.Append(ints[i]);
+                if (i + 1 < ints.Length)
+                {
+                    sb.Append(", ");
+                }
+            }
+            sb.Append(']');
+            return sb.ToString();
+        }
+
+        //For displaying the aboveBelow dictionary contents in console.
+        private static void PrintAboveBelowDictionary(Dictionary<string, int> answer)
+        {
+            answer.Keys.ToList().ForEach(key =>
+            {
+                Console.WriteLine($"{key} : {answer[key]}");
+            });
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RTS_Test
+{
+    public static class Test
+    {        
+        public static Dictionary<string, int> aboveBelow(int[] ints, int value)
+        {
+            //create counter variables for above and below, initialized to 0.
+            int aboveCount = 0;
+            int belowCount = 0;
+
+            //loops through array 
+            foreach (int element in ints)
+            {
+                //increment above or below count when the element is above or below the passed value, respectively
+                if (element > value)
+                {
+                    aboveCount++;
+                }
+                else if (element < value) //cant be just else, as equal values would be counted
+                {
+                    belowCount++;
+                }
+                //do nothing if element == value
+            }
+
+            /*
+              If the passed array was sorted, you could perform a binary search (logn) to find the point at which
+              every element is below and the point at which every element is above (not necessarily the same, as
+              some elements could be equal) and simply find the distance between the 'last below and the start'
+              for the below count and the 'first above and the length' for the above count.  This method is faster,
+              as we would not always need to loop through the entire array.
+            */
+
+            //create answer dictionary and add key/value pairs ofr the above and below counts
+            Dictionary<string, int> answer = new Dictionary<string, int>();
+            answer.Add("above", aboveCount);
+            answer.Add("below", belowCount);
+
+            return answer;
+        }
+
+        public static string stringRotation(string word, int rotation)
+        {
+            //check to see if we can save time if word is empty or isn't being rotated (also error protects later opterations)
+            if (String.IsNullOrEmpty(word) || rotation < 1)
+            {
+                //maybe throw InvalidArgumentException?
+                return word;
+            }
+
+            //create a character array the same size as the string, as we can't edit the string itself (immutable)
+            char[] characterArray = new char[word.Length];
+
+            //create int for holding the index of the character we're swapping to
+            int destinationIndex;
+
+            //loop through each character in the word
+            for (int i = 0; i < word.Length; i++)
+            {
+                //calculate the spot in the new array which will receive the character in the word at the current index
+                destinationIndex = (i + rotation) % word.Length;
+
+                //place the character in the string at the current index into the characterArray at its new destination index
+                //this operation is protected from the empty check earlier
+                characterArray[destinationIndex] = word[i];
+            }
+
+            return new string(characterArray);
+        }
+    }
+}
